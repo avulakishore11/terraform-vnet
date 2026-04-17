7fab7261813ec2680f6750efb8e5f97531dc8dde
kishore avula <avulakishore99@gmail.com>
2026-04-17 13:25:36 -0400

    resource group name got modified from bicep-rg to terraform-rg in main.tf file to terraform-rg.

--- main.tf
+++ main.tf
@@ -23,16 +23,21 @@ provider "azurerm" {
 
 }
 
+resource" resource_group" "rg" {
+  name     = "terraform-rg"
+  location = "eastus"
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet"
   location            = "eastus"
-  resource_group_name = "bicep-rg"
+  resource_group_name = "terraform-rg"
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet"
-  resource_group_name  = "bicep-rg"
+  resource_group_name  = "terraform-rg"
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
