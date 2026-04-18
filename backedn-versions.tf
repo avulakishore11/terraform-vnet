cc086ee109dd30401177ddeb2ac79fe49589d472
kishore avula <avulakishore99@gmail.com>
2026-04-18 14:15:01 -0400

    fixed: backedn storage key got changed

--- backedn-versions.tf
+++ backedn-versions.tf
@@ -3,6 +3,6 @@ terraform {
         resource_group_name  = "rg-tf-state"        # replace with your resource group name where the storage account is located
         storage_account_name = "terrastatesa"       # replace with your storage account name
         container_name       = "tfstate"            # replace with your container name
-        key                  = "windows-vm.tfstate" # replace with your desired state file name
+        key = "dev/terraform.tfstate" # replace with your desired state file name
     }
 }
